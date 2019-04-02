83e4483ae363a88e578858481aac941f3d5313d0
patri <patricio.fernandezflorez@alum.uca.es>
2019-04-02 18:59:29 +0200

    GRAFICAS

--- resources/views/generacionGrafica/welcome.blade.php
+++ resources/views/generacionGrafica/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.js"></script>
+
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -81,7 +84,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <div id="placeholder">
+
+                    </div>
                 </div>
 
                 <div class="links">
@@ -97,3 +102,24 @@
         </div>
     </body>
 </html>
+<script>
+    $( document ).ready(function() {
+        window.onload = function () {
+
+            var d1 = [];
+            for (var i = 0; i < 14; i += 0.5) {
+                d1.push([i, Math.sin(i)]);
+            }
+
+            var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+            // A null signifies separate line segments
+
+            var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+
+            $.plot("#placeholder", [ d1, d2, d3 ]);
+
+
+        }
+    });
+</script>
