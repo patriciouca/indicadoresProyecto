4bf19d1be590ff8a7e6ee8a7a3fc0fe65a816bb2
patri <patricio.fernandezflorez@alum.uca.es>
2019-04-02 15:39:29 +0200

    TPODO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Hola
                 </div>
 
                 <div class="links">
