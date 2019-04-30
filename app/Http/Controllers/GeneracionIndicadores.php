898324ae83e75e99f64b1b77f419488f8cb4a565
patri <patricio.fernandezflorez@alum.uca.es>
2019-04-30 18:52:30 +0200

    repre

--- app/Http/Controllers/GeneracionIndicadores.php
+++ app/Http/Controllers/GeneracionIndicadores.php
@@ -11,7 +11,13 @@ class GeneracionIndicadores extends Controller
 {
     public function index() {
         $tablas = DB::select('SHOW TABLES');
-        return view('generacionIndicadores/welcome')->with('tablas', $tablas);
+        $contador=0;
+        foreach ($tablas as $valor)
+            $devolver[$contador][0] = head($tablas[$contador]);
+            $devolver[$contador][1] = $this->getCampos($devolver[$contador][0]);
+            $contador++;
+            
+        return view('generacionIndicadores/welcome')->with('tablas', $devolver);
     }
 
     public function getCampos($nombre)
--- resources/views/generacionIndicadores/welcome.blade.php
+++ resources/views/generacionIndicadores/welcome.blade.php
@@ -9,82 +9,17 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/base/jquery-ui.css" type="text/css" media="all" />
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js" type="text/javascript"></script>
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
         <script>
 
             $(document).ready(function() {
-                $( "#accordion" )
-                    .accordion({
-                        header: "> div > h3"
-                    })
-                    .sortable({
-                        axis: "y",
-                        handle: "h3",
-                        stop: function( event, ui ) {
-                            // IE doesn't register the blur when sorting
-                            // so trigger focusout handlers to remove .ui-state-focus
-                            ui.item.children( "h3" ).triggerHandler( "focusout" );
+                $( "#accordion" ).accordion({
+                    collapsible: true
+                });
 
-                            // Refresh accordion to handle new order
-                            $( this ).accordion( "refresh" );
-                        }
-                    });
-
+                /*
                 var url="{{Request::url()}}";
                 $( "div.tabla" ).click(function(event) {
                     texto=$($(event.target).find("h3").context).text();
@@ -99,7 +34,7 @@
                         cadena+="</ul></div>";
                         $(event.target).append(cadena)
                     });
-                });
+                });*/
             });
 
         </script>
