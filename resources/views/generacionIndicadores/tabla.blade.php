4ddf6e6d109464b65bf40c75fe7bed5ecc6d1ed7
Miny <davidminybboy@gmail.com>
2019-05-15 17:00:20 +0200

    graf

--- resources/views/generacionIndicadores/tabla.blade.php
+++ resources/views/generacionIndicadores/tabla.blade.php
@@ -13,7 +13,7 @@
                 integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E="
                 crossorigin="anonymous"></script>
 
-        <title>Generador de Indicadores Automáticos</title>
+        <<title>Generador de Indicadores Automáticos</title>
         <link rel="shortcut icon" href={{URL::asset('img/logo1.ico')}}/>
 
         <!-- Fonts -->
@@ -25,25 +25,64 @@
         <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
         <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" type="text/css" media="all" />
 
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.0.3/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.0.3/js/buttons.print.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.0.3/js/buttons.flash.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+        <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+        <script type="text/javascript" src="https://code.highcharts.com/highcharts.js"></script>
         <script>
-
             $(document).ready(function() {
-                var table=$('#tabla1').DataTable({
-                    buttons: [
-                        'copy', 'excel', 'pdf'
-                    ]});
+                var table = $('#tabla1').DataTable( {
+                    dom: 'lftBipr',
 
-                table.buttons().container().appendTo('body');
+                        buttons: [
+                            {
+                                text: 'Generar gráfica',
+                                action: function(){document.myform.submit();}
+                            }
+                        ]
+                    } );
             });
+        </script>
+        <script>
+            var numero=1;
+            function funcion(tam) {
+                switch(numero) {
+                    case 1:$('#ejex').val($('#ejex').val() + tam+",");
+                        numero = 2;
+                        break;
+                    case 2:
+                        $('#ejey').val($('#ejey').val() + tam+",");
+                        numero = 1;
+                        break;
+                }
+            }
         </script>
     </head>
     <body>
 
         <div class="flex-center position-ref full-height">
             <div class="contenidotablas">
+                <form name = "myform" method="POST" style="display: inline" action="{{ url("/generacionGrafica")}}">
+                    {{csrf_field()}}
+                    <div class="botones"></div>
+                    <input id="consulta" type="text" value="{{$consulta}}" disabled>
+                    <input hidden id="ejex" name="ejex" type="text">
+                    <input hidden id="ejey" name = "ejey" type="text">
+                </form>
                 <div class="botones"></div>
+
                 <input id="consulta" type="text" value="{{$consulta}}" disabled>
                 <table id="tabla1" class="table-striped  table-hover table">
+
+                        <div class="botones"></div>
+                    <input id="consulta" type="text" value="{{$consulta}}" disabled>
+
 
                     <thead>
                         <tr>
@@ -57,12 +96,16 @@
                         @foreach($tablas as $key=>$tabla)
                             <tr>
                             @foreach($tabla as $tam)
-                                    <td>{{$tam}}</td>
+                                    <td>{{$tam}}
+                                        <script>
+                                            funcion('{{$tam}}');
+                                        </script></td>
                             @endforeach
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
+            </div>
 
             </div> </div>
 
