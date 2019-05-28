e95841557277fe7c5970edcd1acc4de75a72c1de
Antonio Rivas <e4a.rivas26@gmail.com>
2019-05-29 00:41:20 +0200

    WelcomeBlade + gifs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,13 +48,15 @@
                     <div class="articleBody clear">
                     
                     	<figure>
-                            <img src={{ URL::asset('img/ejemplo.gif') }}  width="620" height="340" alt="">
+                            <img src={{ URL::asset('img/gif1.gif') }}  width="640" height="339" alt="">
                         </figure>
-                        
-                        <p>En primer lugar debe seleccionar la tabla en la que se encuentre el indicador que desea utilizar. Una vez pulse sobre la tabla se desplegarán los distintos campos que esta contiene, en este momento deberá arrastrar el indicador deseado al eje en el que desee colocarlo.</p>
-                        <p>Recuerde que puede hacer operaciones entre los distintos indicadores como son: la suma, la multiplicación, la resta, y la división. </p>
-						<p>También es posible contar el número de elementos existentes para un indicador determinado, utilizando la operación contar. </p>
-						<p>NOTA: Recuerde cerrar el paréntesis de la operación contar cuando finalice esta.</p>
+
+                        <p>Antes de comenzar deberá elegir sobre que base de datos, de las disponibles en su sistema, se generarán los indicadores </p>
+                        <p>En primer lugar debe seleccionar la tabla en la que se encuentre el indicador que desea utilizar. Una vez pulse sobre la tabla se desplegarán los distintos campos que esta contiene, cada campo indica el tipo de dato que maneja, en este momento deberá arrastrar el indicador deseado al eje en el que desee colocarlo tal y como puede ver en la imagen de la derecha.</p>
+                        <p>Recuerde que puede hacer operaciones matemáticas entre los distintos indicadores tan solo arastrandolas y si lo desea, borrar arrastrando el elemento "borrar". </p>
+						<p>También es posible contar el número de elementos existentes para un indicador determinado, utilizando la operación "contar" sobre un elemento numérico. </p>
+                        <p>Puede realizar busquedas de tablas por nombre utilizando el buscador situado en la parte superior izquierda y arrastrar estas mismas tablas en el eje vertical a su gusto.</p>
+                        <p>NOTA: Recuerde cerrar el paréntesis de la operación "contar" cuando finalice esta y que los operadores de comparacion solo son válidos para los filtros.</p>
 						<p>Para finalizar pulse sobre "Generar consulta" </p>
                     </div>
                 </article>
@@ -69,17 +71,37 @@
                     <div class="articleBody clear">
                     
                     	<figure>
-	                    	<img src={{ URL::asset('img/ejemplo.gif') }} width="620" height="340" />
+	                    	<img src={{ URL::asset('img/gif22.gif') }} width="640" height="339" />
                         </figure>
                        
-						<p>Una vez se hayan generado los indicadores, aparecerán en una tabla para que los puedas interpretar. </p>
-						<p>Tambien tienes la opción de generar una gráfica con estos </p>
-						<p>NOTA: Recuerde cerrar el paréntesis de la operación contar cuando finalice esta.</p>
-						<p>Para finalizar pulse sobre "Generar consulta" </p>
-						<p>NOTA: Recuerde cerrar el paréntesis de la operación contar cuando finalice esta.</p>
-						<p>Para finalizar pulse sobre "Generar consulta" </p>
-						<p>NOTA: Recuerde cerrar el paréntesis de la operación contar cuando finalice esta.</p>
-						<p>Para finalizar pulse sobre "Generar consulta" </p>
+						<p>Una vez se hayan generado los indicadores, aparecerán en una tabla. </p>
+						<p>En la parte superior puede ver la consulta SQL que se ha utilizado para generarlos. </p>
+						<p>Bajo esta puede ber dos grupos de botones, los 4 primeros son para exportar la tabla en distintos formatos como son copiada en el portapapeles, en formato Excel, en PDF y en imagen para imprimir, en orden de izquierda a derecha, y el último botón es el que generará una gráfica con estos indicadores.</p>
+						<p>En cuanto a la tabla puede realizar acciones como ordenar las distintas filas o realizar una búsqueda entre estas utilizando el buscador situado en la parte superior derecha de la tabla. </p>
+
+                    </div>
+                </article>
+
+                <div class="line"></div>
+
+                <article id="article1">
+                    <h2>¿Cómo manejar la gráfica?</h2>
+
+                    <div class="line"></div>
+
+                    <div class="articleBody clear">
+
+                        <figure>
+                            <img src={{ URL::asset('img/gif3.gif') }} width="640" height="339" />
+                        </figure>
+
+                        <p>Una vez se genere la tabla puede realizar cámbios de estilo sobre ella, por defecto aparecerá representada con lineas, puntos, barras y relleno. </p>
+                        <br>
+                        <p>Estas cuatro modalidades de representación de la gráfica son las que puede utilizar y combinar a su gusto tan solo haciando clic sobre el recuadro correspondiente. </p>
+                        <br>
+                        <p>También puede variar a su gusto el color de cada uno de estos cuatro elementos, pulsando sobre el rectángulo de color correspondiente.</p>
+                        <br>
+                        <p>Por último tiene la opción de exportar la gráfica en formato PNG pulsando sobre el botón que se encuentra en la parte superior central de la pantalla, sobre la gráfica. </p>
                     </div>
                 </article>
 
